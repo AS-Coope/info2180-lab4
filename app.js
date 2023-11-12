c0a2145607308f2d793d1881b5b80554071c5702
AS-Coope <deltazirconium@gmail.com>
2023-11-12 17:51:00 -0500

    Setup up request and response from the server

--- app.js
+++ app.js
@@ -2,13 +2,32 @@ document.addEventListener("DOMContentLoaded", (event) => {
     main();
 });
 
+const httpRequest = new XMLHttpRequest();
+let resourceUrl = "http://localhost/info2180-lab4/superheroes.php";
+
+// make request to the server
 function makeAjaxRequest() {
-    const httpRequest = new XMLHttpRequest();
-    let resourceUrl = "http://localhost/info2180-lab4/superheroes.php";
-    httpRequest.onreadystatechange = console.log("Initial Test");
+
+    const searchBtn = document.getElementById("searchBtn");
+    httpRequest.onreadystatechange = searchBtn.addEventListener("click", (event) => {
+        returnAvengers();
+    });
     httpRequest.open("GET", resourceUrl);
     httpRequest.send();
 }
+
+// retrieve response from the server
+function returnAvengers() {
+    if (httpRequest.readyState === XMLHttpRequest.DONE) {
+        if (httpRequest.status === 200) {
+            let response = httpRequest.responseText;
+            alert(response);
+        } else {
+            alert("Error: Could not load Avengers data!");
+        }
+    }
+}
+
 function main() {
     makeAjaxRequest();
 }
