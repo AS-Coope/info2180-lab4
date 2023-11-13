64af2ebc515149a0f3eb26d93a2104ce3138c5a6
AS-Coope <deltazirconium@gmail.com>
2023-11-12 21:01:37 -0500

    Start creation of form validation checks

--- app.js
+++ app.js
@@ -4,13 +4,29 @@ document.addEventListener("DOMContentLoaded", (event) => {
 
 const httpRequest = new XMLHttpRequest();
 const resultDiv = document.getElementById("result");
+const searchField = document.getElementById("searchField");
+const searchBtn = document.getElementById("searchBtn");
+const form = document.querySelector("form");
 let resourceUrl = "http://localhost/info2180-lab4/superheroes.php";
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault() // stop the form from submitting
+    if (checkEmptyString(searchField.value.trim())) {
+        // if the tests prove true, then submit data
+    } else {
+        // inform the user that what was entered is invalid/ not found in the 'database/array'
+    }
+});
+
+function checkEmptyString(enteredData) {
+    return enteredData.length === 0 ? true : false;
+}
+
 
 // make request to the server
 function makeAjaxRequest() {
 
-    const searchBtn = document.getElementById("searchBtn");
-    httpRequest.onreadystatechange = searchBtn.addEventListener("click", (event) => {
+    httpRequest.onreadystatechange = form.addEventListener("click", (event) => {
         returnAvengers();
     });
     httpRequest.open("GET", resourceUrl);
@@ -18,7 +34,7 @@ function makeAjaxRequest() {
 }
 
 // retrieve response from the server
-function returnAvengers() {
+function returnAvengers(trimmedEntry) {
     if (httpRequest.readyState === XMLHttpRequest.DONE) {
         if (httpRequest.status === 200) {
             let response = httpRequest.responseText;
